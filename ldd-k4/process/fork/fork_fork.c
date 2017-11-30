d9a6031a5038955f0e7676e41d11a9c935e28b42
ChanningLan <channinglan@ieiworld.com>
2017-11-30 17:37:41 +0800

    update

--- ldd-k4/process/fork/fork_fork.c
+++ ldd-k4/process/fork/fork_fork.c
@@ -4,19 +4,34 @@
 
 void sigint_handle(int sig)
 {
-	printf("got the %d signal...\n", sig);
+	printf("<pid:%d>got the %d signal...\n",getpid(), sig);
 	exit(0);
 }
 
 int main(int argc, char* argv[])
 {
+	int pid1,pid2,pid3,ch=0;
 	if(signal(SIGINT, sigint_handle) == SIG_ERR){
 		perror("cannot reset the SIGINT signal handler");
 		return 1;
 	}
 
-	fork();
-	fork();
-	printf("pid:%d hello world\n", getpid());
+	pid1 = fork();
+	printf("<pid:%d>pid1=%d\n", getpid(),pid1);	
+	if(pid1==0) {
+			ch+=1;
+	}
+	pid2 = fork();
+	printf("<pid:%d>pid2=%d\n", getpid(),pid2);
+	if(pid2==0) {
+			ch+=2;
+	}
+	pid3 = fork();
+	printf("<pid:%d>pid3=%d\n", getpid(),pid3);
+	if(pid3==0) {
+			ch+=4;
+	}
+	
+	printf("<pid:%d><ch=%d>hello world\n",getpid(),ch);
 	while(1);
 }
